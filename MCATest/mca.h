e2e1353c41df62ad3b448012218acb0e35da46dd
Conner Wiench <connerdwiench@gmail.com>
2024-03-07 17:40:47 -0800

    Tested and approved the MCA module.

--- MCATest/mca.h
+++ MCATest/mca.h
@@ -1,32 +1,37 @@
+#include "usb_serial.h"
+#include "core_pins.h"
 
 #define CAPTURE_SIZE 4096
 #define TIMEOUT_SECONDS 30
+
+#include "HardwareSerial.h"
+#include <stdio.h>
 
 class MCA{
   public:
     uint32_t recentCapture[CAPTURE_SIZE];
 
+    void clear_recent();
+    void zero_out();
+    uint32_t capture();
+    MCA(HardwareSerial *ser);
   private:
     uint32_t startTime;
     HardwareSerial *mSerial;
 
-    MCA(*HardwareSerial);
-    void zero_out();
-    void clear_recent();
-    uint32_t capture();
 };
 
 MCA::MCA(HardwareSerial *ser){
   mSerial = ser;
   mSerial->begin(115200);
 
-  zero_out();
+  // zero_out();
   clear_recent();
 }
 
 // Zeros out the MCA to reset collected data.
 void MCA::zero_out(){
-  mSerial->write(0); mSerial->write(0); // Get actual zero out bits 
+  mSerial->write(1); mSerial->write(1); // Get actual zero out bits 
 }
 
 void MCA::clear_recent(){
@@ -36,19 +41,23 @@ void MCA::clear_recent(){
 }
 
 uint32_t MCA::capture(){
-  timeout = millis() + (1000 * TIMEOUT_SECONDS); // Sets timeout if not all/any data can be gotten from MCA
+  uint timeout = millis() + (1000 * TIMEOUT_SECONDS); // Sets timeout if not all/any data can be gotten from MCA
 
-  mSerial->write(0); mSerial->write(0); // Send data request to MCA
-  while((!mSerial.available) && (timeout > millis())){} // Wait for a response with timeout
+  mSerial->write((int)0); mSerial->write(16); // Send data request to MCA
+  while((!mSerial->available()) && (timeout > millis())){} // Wait for a response with timeout
 
   int hold = 0;
-  for(int i = 0; (i < CAPTURE_SIZE) && (timeout > millis()), ++i){
+  for(int i = 0; (i < CAPTURE_SIZE) && (timeout > millis()); ++i){
     hold = mSerial->read();
     if(hold <= -1){
       --i;
       continue;
     }
-    recentCapture[0] = hold;
+    recentCapture[i] = hold;
+  }
+  if(millis() >= timeout){
+    Serial.println("Capture Timed out...");
+    return 2;
   }
 
   return 1;
