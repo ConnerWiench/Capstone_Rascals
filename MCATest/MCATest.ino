1b5618cdc1ef6618ed38cc125aeff8dc1fa8e1b3
Conner Wiench <connerdwiench@gmail.com>
2024-03-19 20:09:47 -0700

    Updated MCA code with simple command interface and error checking.

--- MCATest/MCATest.ino
+++ MCATest/MCATest.ino
@@ -9,69 +9,37 @@ MCA mca(&Serial1);
 void setup(){
   Serial.begin(9600);
 
-  Serial.println("MCA Started waiting...");
+  Serial.println("MCA Starting...");
 
-  // delay(10000);
+  delay(5000);
 
-  Serial.println("Done Waiting, Capturing.");
-  uint err = mca.capture();
-  Serial.println("Done Capturing Data...");
-
-  if(err == 2){
-    Serial.println("Capture Timed Out");
-    exit(-1);
-  }
-
-  for(int i = 0; i < 4096; ++i){
-    // Serial.print(i); Serial.print(": "); Serial.println(mca.recentCapture[i]); // Labeled
-    Serial.print(mca.recentCapture[i]); Serial.print(',');
-  }
-  Serial.println("\n Done!");
+  Serial.println("MCA Started.");
 }
 
 void loop(){
+  if(Serial.available()){
+    char hold = Serial.read();
 
+    if(hold == 'z'){
+      mca.zero_out();
+      Serial.println("MCA Zeroed Out");
+    }
+    else{
+      Serial.println("Starting Data Capture");
+      uint err = mca.capture();
+      Serial.println("Done Capturing Data...");
+
+      if(err == 2){
+        Serial.println("Capture Timed Out");
+      }
+
+      for(int i = 0; i < mca.captureSize; ++i){
+        // Serial.print(i); Serial.print(": "); Serial.println(mca.recentCapture[i]); // Labeled
+        Serial.print(mca.recentCapture[i]); Serial.print(',');
+      }
+      Serial.println("\n Done!");
+    }
+
+    Serial.clear();
+  }
 }
-// #define ARR_SIZE 4096
-
-// void setup() {
-//   Serial.begin(9600);
-//   Serial1.begin(115200);
-
-//   Serial.println("Setup Complete");
-// }
-
-// void loop() {
-//   static String input = "";
-//   static uint32_t output[ARR_SIZE];
-//   static uint index = 0;
-
-//   if(Serial.available()){
-//     while(Serial.available()){
-//       input += Serial.read();
-//     }
-//     Serial.print("Input: "); Serial.println(input);
-//     Serial1.write(0); Serial1.write(16); // Serial1.print(input); 
-
-//     input = "";
-
-//     for(int i = 0; i < ARR_SIZE; ++i){
-//       output[i] = 0;
-//     }
-//   }
-
-//   if(Serial1.available()){
-//     Serial.println("Receiving MCA Data");
-
-//     output[index] = Serial1.read();
-//     index++;
-
-//     Serial.println("Output: ");
-//     for(int i = 0; i < ARR_SIZE; ++i){
-//       Serial.print(output[i]); Serial.print(',');
-//     }
-//     Serial.println("\n");
-
-//     Serial.println("Read All MCA Data");
-//   }
-// }
--- MCATest/mca.h
+++ MCATest/mca.h
@@ -1,8 +1,9 @@
+#include <stdint.h>
 #include "usb_serial.h"
 #include "core_pins.h"
 
-#define CAPTURE_SIZE 4096
-#define TIMEOUT_SECONDS 30
+#define CAPTURE_SIZE 4160
+#define TIMEOUT_SECONDS 10
 
 #include "HardwareSerial.h"
 #include <stdio.h>
@@ -10,6 +11,7 @@
 class MCA{
   public:
     uint32_t recentCapture[CAPTURE_SIZE];
+    const int captureSize = CAPTURE_SIZE;
 
     void clear_recent();
     void zero_out();
@@ -25,7 +27,6 @@ MCA::MCA(HardwareSerial *ser){
   mSerial = ser;
   mSerial->begin(115200);
 
-  // zero_out();
   clear_recent();
 }
 
@@ -34,20 +35,22 @@ void MCA::zero_out(){
   mSerial->write(1); mSerial->write(1); // Get actual zero out bits 
 }
 
+// Clears the recentCapture array with all 0's.
 void MCA::clear_recent(){
-  for(int i = 0; i < CAPTURE_SIZE; ++i){
+  for(int i = 0; i < captureSize; ++i){
     recentCapture[i] = 0;
   }
 }
 
 uint32_t MCA::capture(){
-  uint timeout = millis() + (1000 * TIMEOUT_SECONDS); // Sets timeout if not all/any data can be gotten from MCA
+  uint timeout = millis() + (1000 * TIMEOUT_SECONDS); // Sets timeout if not all/any data can be read from the MCA.
 
-  mSerial->write((int)0); mSerial->write(16); // Send data request to MCA
+  while(mSerial->available()){mSerial->read();} // Clear out any pre-existing data in the buffer
+  mSerial->write((int)0); mSerial->write((int)16); // Send data request to MCA
   while((!mSerial->available()) && (timeout > millis())){} // Wait for a response with timeout
 
   int hold = 0;
-  for(int i = 0; (i < CAPTURE_SIZE) && (timeout > millis()); ++i){
+  for(int i = 0; (i < captureSize) && (timeout > millis()); ++i){ // Reads out the serial data against a count instead of if Serial is available.  Also has timeout
     hold = mSerial->read();
     if(hold <= -1){
       --i;
@@ -55,9 +58,10 @@ uint32_t MCA::capture(){
     }
     recentCapture[i] = hold;
   }
+
   if(millis() >= timeout){
     Serial.println("Capture Timed out...");
-    return 2;
+    return 2;  // Return error value if timed out.
   }
 
   return 1;
