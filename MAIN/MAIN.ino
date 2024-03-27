0750032db03894308fd34049b329f5a90119bad7
Conner Wiench <connerdwiench@gmail.com>
2024-03-27 14:20:04 -0700

    Updated main program with addition Serial Calls

--- MAIN/MAIN.ino
+++ MAIN/MAIN.ino
@@ -32,39 +32,51 @@ void loop() {
     byte opcode = 0;
     opcode = RS422_SERIAL.read();
 
-    switch (opcode) {  // Respond to a check if we're alive.
-      case 1:
+    switch(opcode){ // Respond to a check if we're alive.
+
+      case 1: // Response to Hello message.
+        Serial.println("Received Hello Message");
         RS422_SERIAL.write(2);
         return;
-      case 3:
+
+      case 3: // Zero's scintillator and sets the next read by time.
+      { // Need braces to seperate scope for timeLength.  Else causes error.
         if(!RS422_SERIAL.available()){
-          Serial.print("Expected Additional Data");
+          Serial.print("Error: Expected Additional Data");
           return;
         }
         byte timeLength = RS422_SERIAL.read();
         mcaRuntime = millis() + (timeLength * 1000 * 60); // Set runtime by 'timeLength' minutes.
+        Serial.printf("Received Timed MCA Instruction, Length=%d\n", timeLength);
         mca.zero_out();
+        return;
+      }
+
+      case 5: // Send current MCA data.
+        for(int i = 0; i < mca.captureSize; ++i){
+          RS422_SERIAL.write(mca.recentCapture[i]);
+        }
+        RS422_SERIAL.write(254);
+        return;
+
+      case 7: // Zero out MCA without auto-read.
+        Serial.println("Instant Zero Out MCA");
+        mca.zero_out();
+        RS422_SERIAL.write(8);
+        return;
+
+      case 9: // Pull more data from MCA without zero-ing it out.
+        Serial.println("Instantly pulling data from MCA");
+        mca.capture();
+        RS422_SERIAL.write(10);
         return;
     }
   }
 
   if(mcaRuntime && millis() > mcaRuntime){
-    mcaRuntime = millis() + (1 * 1000 * 60);  // Create 1 minute timeout for waiting.
+    mcaRuntime = 0;
+    mca.capture();
     RS422_SERIAL.write(4);  // Send interface code for MCA ready.
-
-    while(!(RS422_SERIAL.available()) && (millis() < mcaRuntime)){} // Wait for a response message to come in.
-    byte response = RS422_SERIAL.read();
-
-    if(response == 5){
-      mca.capture();
-      for(int i = 0; i < CAPTURE_SIZE; ++i){
-        RS422_SERIAL.write(mca.recentCapture[i]);
-      }
-      RS422_SERIAL.write(-2);
-    }
-    else{
-      RS422_SERIAL.write(-3);
-    }
   }
 
 }
